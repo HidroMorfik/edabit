4ecbeb9900e942c10e8cb2e7639839b22362892c
HidroMorfik <wildones.zafer@gmail.com>
2021-06-29 22:46:55 +0300

    power-calculator.py

---
+++ edabit/01-very-easy/power-calculator.py
@@ -0,0 +1,24 @@
+"""
+Power Calculator
+Create a function that takes voltage and current and returns the calculated power.
+
+Examples
+circuit_power(230, 10) ➞ 2300
+
+circuit_power(110, 3) ➞ 330
+
+circuit_power(480, 20) ➞ 9600
+Notes
+Requires basic calculation of electrical circuits (see Resources for info).
+"""
+from edabit.Test import Test
+
+
+def circuit_power(voltage, current):
+    return voltage * current
+
+
+if __name__ == '__main__':
+    Test.assert_equals(circuit_power(110, 3), 330)
+    Test.assert_equals(circuit_power(230, 10), 2300)
+    Test.assert_equals(circuit_power(480, 20), 9600)
