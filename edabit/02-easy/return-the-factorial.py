cbdd5af5fad09c19f7b7ef1fe2bce43041816ca8
HidroMorfik <wildones.zafer@gmail.com>
2021-07-16 16:24:20 +0300

    return-the-factorial.py

---
+++ edabit/02-easy/return-the-factorial.py
@@ -0,0 +1,28 @@
+"""
+Return the Factorial
+Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+
+Examples
+factorial(3) ➞ 6
+
+factorial(5) ➞ 120
+
+factorial(13) ➞ 6227020800
+Notes
+Assume all inputs are greater than or equal to 0.
+"""
+import math
+from edabit.Test import Test
+
+
+def factorial(num):
+    return math.factorial(num)
+
+
+
+if __name__ == '__main__':
+    Test.assert_equals(factorial(2), 2)
+    Test.assert_equals(factorial(6), 720)
+    Test.assert_equals(factorial(3), 6)
+    Test.assert_equals(factorial(12), 479001600)
+    Test.assert_equals(factorial(5), 120)
