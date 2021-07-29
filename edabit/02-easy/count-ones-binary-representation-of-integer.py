e31880e85fd128363c7fa35ab2aebf953a0e0460
HidroMorfik <wildones.zafer@gmail.com>
2021-07-29 21:46:37 +0300

    count-ones-binary-representation-of-integer.py

---
+++ edabit/02-easy/count-ones-binary-representation-of-integer.py
@@ -0,0 +1,30 @@
+"""
+Count Ones in Binary Representation of Integer
+Count the amount of ones in the binary representation of an integer. For example, since 12 is 1100 in binary, the return value should be 2.
+
+Examples
+count_ones(0) ➞ 0
+
+count_ones(100) ➞ 3
+
+count_ones(999) ➞ 8
+Notes
+The input will always be a valid integer (number).
+"""
+
+from edabit.Test import Test
+
+
+def count_ones(num):
+    num2 = bin(num)[2:]       # I got help from my brother
+    return num2.count("1")
+
+
+if __name__ == '__main__':
+    Test.assert_equals(count_ones(12), 2)
+    Test.assert_equals(count_ones(0), 0)
+    Test.assert_equals(count_ones(100), 3)
+    Test.assert_equals(count_ones(101), 4)
+    Test.assert_equals(count_ones(999), 8)
+    Test.assert_equals(count_ones(123456789), 16)
+    Test.assert_equals(count_ones(1234567890), 12)
