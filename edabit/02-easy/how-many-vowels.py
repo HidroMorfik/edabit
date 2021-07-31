3e06a06ce0f6dc74e1017219931392b5ba4fc1f0
HidroMorfik <wildones.zafer@gmail.com>
2021-07-31 19:47:15 +0300

    how-many-vowels.py

---
+++ edabit/02-easy/how-many-vowels.py
@@ -0,0 +1,45 @@
+"""
+How Many Vowels?
+Create a function that takes a string and returns the number (count) of vowels contained within it.
+
+Examples
+count_vowels("Celebration") ➞ 5
+
+count_vowels("Palm") ➞ 1
+
+count_vowels("Prediction") ➞ 4
+Notes
+a, e, i, o, u are considered vowels (not y).
+All test cases are one word and only contain letters.
+"""
+
+
+from edabit.Test import Test
+def isVowel(ch):
+    return ch.upper() in ['A', 'E', 'I', 'O', 'U']      # I got help from "www.geeksforgeeks.org"
+
+def count_vowels(txt):
+    count = 0
+    for i in range(len(txt)):
+
+
+        if isVowel(txt[i]):
+            count += 1
+    return count
+
+
+
+
+
+
+if __name__ == '__main__':
+    Test.assert_equals(count_vowels("Celebration"), 5)
+    Test.assert_equals(count_vowels("Palm"), 1)
+    Test.assert_equals(count_vowels("Prediction"), 4)
+    Test.assert_equals(count_vowels("Suite"), 3)
+    Test.assert_equals(count_vowels("Quote"), 3)
+    Test.assert_equals(count_vowels("Portrait"), 3)
+    Test.assert_equals(count_vowels("Steam"), 2)
+    Test.assert_equals(count_vowels("Tape"), 2)
+    Test.assert_equals(count_vowels("Nightmare"), 3)
+    Test.assert_equals(count_vowels("Convention"), 4)
