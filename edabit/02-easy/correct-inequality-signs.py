73fc7cf3776e94c42d61027e9f6cfe70caebb749
HidroMorfik <wildones.zafer@gmail.com>
2021-08-05 15:10:14 +0300

    correct-inequality-signs.py

---
+++ edabit/02-easy/correct-inequality-signs.py
@@ -0,0 +1,32 @@
+"""
+Correct Inequality Signs
+Create a function that returns True if a given inequality expression is correct and False otherwise.
+
+Examples
+correct_signs("3 < 7 < 11") ➞ True
+
+correct_signs("13 > 44 > 33 > 1") ➞ False
+
+correct_signs("1 < 2 < 6 < 9 > 3") ➞ True
+Notes
+N/A
+"""
+
+from edabit.Test import Test
+
+
+def correct_signs(txt):
+    if eval(txt) is True:          # I learned something myself
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    Test.assert_equals(correct_signs("3 < 7 < 11"), True)
+    Test.assert_equals(correct_signs("13 > 44 > 33 > 1"), False)
+    Test.assert_equals(correct_signs("1 < 2 < 6 < 9 > 3"), True)
+    Test.assert_equals(correct_signs("4 > 3 > 2 > 1"), True)
+    Test.assert_equals(correct_signs("5 < 7 > 1"), True)
+    Test.assert_equals(correct_signs("5 > 7 > 1"), False)
+    Test.assert_equals(correct_signs("9 < 9"), False)
